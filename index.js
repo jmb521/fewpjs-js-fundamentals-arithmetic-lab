e17b3306c826253f7fd89fdca61eca4b1cea4bb4
Jennifer Pazos <jmp521@gmail.com>
2021-03-01 13:11:55 -0600

    commit tree 3458db821c8fb262f10361c6f06af6b8833d201f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let num3 = 20
+let num4 = 16
+let multiply = num1 * num2
 
-// Write your code here
+let random =parseInt( Math.random() * 10, 10 )
+
+let mod = num3 % num4
+
+
+let max = Math.max(10, 1, 12, 14, 20, 3, 8, 2, 5)
